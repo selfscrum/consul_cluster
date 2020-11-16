1036d7e3bf13a0a531c7903f58742224d5a8edb1
Martin Jahr <martin@desixma.org>
2020-11-16 22:01:56 +0100

    new variable

--- workspace/workspace.tf
+++ workspace/workspace.tf
@@ -2,7 +2,7 @@ locals {
   system = jsondecode(file("assets/system.json"))
 }
 
-resource "tfe_workspace" "boundary_network" {
+resource "tfe_workspace" "consul_cluster" {
   name  = format("%s_%s", 
                 local.system["env_stage"],
                 local.system["workspace"]
@@ -15,7 +15,7 @@ resource "tfe_variable" "ws_access_token" {
     key          = "access_token"
     value        = ""
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "Workspace that created the Boundary Network"
     sensitive    = true
 }
@@ -27,7 +27,7 @@ resource "tfe_variable" "ws_env_name" {
                     random_pet.name.id
                     )
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "Name of the Component"
 }
 
@@ -35,7 +35,7 @@ resource "tfe_variable" "ws_env_stage" {
     key          = "env_stage"
     value        = lookup(local.system, "env_stage")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "Stage of the Component"
 }
 
@@ -43,7 +43,7 @@ resource "tfe_variable" "ws_location" {
     key          = "location"
     value        = lookup(local.system, "location")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "Location of the Component"
 }
 
@@ -51,7 +51,7 @@ resource "tfe_variable" "ws_system_function" {
     key          = "system_function"
     value        = lookup(local.system, "system_function")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "System Function of the Component"
 }
 
@@ -59,7 +59,7 @@ resource "tfe_variable" "ws_consul_image" {
     key          = "consul_image"
     value        = lookup(local.system, "consul_image")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "consul_image of the Component"
 }
 
@@ -67,7 +67,7 @@ resource "tfe_variable" "ws_consul_type" {
     key          = "consul_type"
     value        = lookup(local.system, "consul_type")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "consul_type of the Component"
 }
 
@@ -75,7 +75,7 @@ resource "tfe_variable" "ws_keyname" {
     key          = "keyname"
     value        = lookup(local.system, "keyname")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "keyname of the Component"
 }
 
@@ -83,7 +83,7 @@ resource "tfe_variable" "ws_network_zone" {
     key          = "network_zone"
     value        = lookup(local.system, "network_zone")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "network_zone of the Component"
 }
 
@@ -91,6 +91,6 @@ resource "tfe_variable" "ws_network_component" {
     key          = "network_component"
     value        = lookup(local.system, "network_component")
     category     = "terraform"
-    workspace_id = tfe_workspace.boundary_network.id
+    workspace_id = tfe_workspace.consul_cluster.id
     description  = "Hetzner Network Component where the new component will reside in"
 }
